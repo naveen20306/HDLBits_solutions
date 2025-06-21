d4028c99375402ca50179e105456762c3d422999
naveen20306 <naveenau2023@gmail.com>
2025-06-21 23:34:51 +0530

    Added problem5-norgate verilog and testbench code

--- module_shift8/norgate.v
+++
@@ -1,6 +0,0 @@
-module norgate( // Module for a NOR gate.
-    input a, // First input.
-    input b, // Second input.
-    output out ); // Output of the NOR gate.
-    assign out = ~(a || b); // Assigns the logical NOR of 'a' and 'b' to 'out'.
-endmodule // End of module.
